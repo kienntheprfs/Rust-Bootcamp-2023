f6435354c082ea10beb58ee5562bd9fb209bdbbe
kienntheprfs <kien.trantrung@hcmut.edu.vn>
2023-07-10 22:26:19 +0700

    Done!

--- exercises/ownership-borrowing/src/lib.rs
+++ exercises/ownership-borrowing/src/lib.rs
@@ -1,5 +1,6 @@
 // Exercise 1
 // Make it compile
+#[allow(unused_variables,dead_code)]
 fn exercise1() {
     // Use as many approaches as you can to make it work
     let x = String::from("hello, world");
@@ -10,6 +11,7 @@ fn exercise1() {
 // Exercise 2
 // Make it compile
 // Don't modify code in exercise2 function!
+#[allow(unused_variables,dead_code)]
 fn exercise2() {
     let s1 = String::from("hello, world");
     let s2 = take_ownership(s1);
@@ -25,13 +27,14 @@ fn take_ownership(s: String)->String {
 // Exercise 3
 // Make it compile
 // Dont care about logic
+#[allow(unused_variables,dead_code)]
 fn exercise3() {
     let values: Vec<f64> = vec![
         2817.42, 2162.17, 3756.57, 2817.42, -2817.42, 946.9, 2817.42, 964.42, 795.43, 3756.57,
         139.34, 903.58, -3756.57, 939.14, 828.04, 1120.04, 604.03, 3354.74, 2748.06, 1470.8,
         4695.71, 71.11, 2391.48, 331.29, 1214.69, 863.52, 7810.01,
     ];
-
+    
     let values_number = values.len();
 
     let additions: Vec<usize> = vec![0];
@@ -42,7 +45,7 @@ fn exercise3() {
         let mut addition: f64 = 0.0;
 
         // Sumar valores en additions
-        for element_index in additions {
+        for &element_index in &additions {
             let addition_aux = values[element_index];
             addition = addition_aux + addition;
         }
@@ -51,25 +54,28 @@ fn exercise3() {
 
 // Exercise 4
 // Make it compile
-fn exercise4(value: u32) -> &'static str {
+#[allow(unused_variables,dead_code)]
+fn exercise4(value: u32) ->  String {
     let str_value = value.to_string(); // Convert u32 to String
-    let str_ref: &str = &str_value; // Obtain a reference to the String
+    let str_ref: String = str_value; // Obtain a reference to the String
     str_ref // Return the reference to the String
 }
 
 // Exercise 5
 // Make it compile
+
 use std::collections::HashMap;
+#[allow(unused_variables,dead_code)]
 fn exercise5() {
     let mut my_map = HashMap::from([(1, "1.0".to_string()), (2, "2.0".to_string())]);
 
     let key = 3;
+    let value = "3.0".to_string();
 
-    let res = match my_map.get(&key) {
+    let res: &String = match my_map.get(&key) {
         Some(child) => child,
         None => {
-            let value = "3.0".to_string();
-            my_map.insert(key, value);
+            my_map.insert(key, value.clone());
             &value // HERE IT FAILS
         }
     };
@@ -81,26 +87,28 @@ fn exercise5() {
 // Make it compile
 
 use std::io;
-
+#[warn(unused_assignments,dead_code)]
 fn exercise6() {
-    let mut prev_key: &str = "";
-
+    
     for line in io::stdin().lines() {
         let s = line.unwrap();
-
+        
+        let mut prev_key: &str = "";
         let data: Vec<&str> = s.split("\t").collect();
         if prev_key.len() == 0 {
             prev_key = data[0];
+            println!("{}",prev_key);
         }
     }
 }
 
 // Exercise 7
 // Make it compile
+#[allow(unused_variables,dead_code)]
 fn exercise7() {
+    let chars = [b'x', b'y', b'z'];
     let mut v: Vec<&str> = Vec::new();
     {
-        let chars = [b'x', b'y', b'z'];
         let s: &str = std::str::from_utf8(&chars).unwrap();
         v.push(&s);
     }
@@ -109,15 +117,16 @@ fn exercise7() {
 
 // Exercise 8
 // Make it compile
+#[allow(unused_variables,dead_code)]
 fn exercise8() {
     let mut accounting = vec!["Alice", "Ben"];
     
-    loop {
-        let mut add_input = String::from("");
+    let mut add_input = String::from("");
 
-        io::stdin()
-            .read_line(&mut add_input)
-            .expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut add_input)
+        .expect("Failed to read line");
+    loop {
 
         let add_vec: Vec<&str> = add_input.trim()[..].split_whitespace().collect();
 
@@ -129,4 +138,15 @@ fn exercise8() {
         let person = add_vec[0];
         accounting.push(person);
     }
+}
+#[allow(unused_variables,dead_code)]
+fn main(){
+    exercise1();
+    exercise2();
+    exercise3();
+    exercise4(32);
+    exercise5();
+    exercise6();
+    exercise7();
+    exercise8();
 }
